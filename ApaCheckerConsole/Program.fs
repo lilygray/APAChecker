8a9ab76d13fd1902d2a3ae3a31ad184147ba15ec
Lily Gray <lily.gray1729@gmail.com>
2017-08-08 12:05:13 -0500

    Modularized tests, started working with regex to find numbers in word form

--- ApaCheckerConsole/Program.fs
+++ ApaCheckerConsole/Program.fs
@@ -10,6 +10,7 @@ open System.Xml.Linq
 open System.Xml.XPath
 //open Stanford.NLP.FSharp
 //open HtmlAgilityPack
+open System.Text.RegularExpressions
 
 [<EntryPoint>]
 
@@ -88,9 +89,11 @@ let main argv =
     doc.Load("text.xml")
     let xmlNode = doc.DocumentElement :> XmlNode
 
+    //function to get all one element with a specific name. This way we don't have to manually dial the "area code" every time.
     let getElement elementName =
         xmlNode.SelectSingleNode("//TEI:" + elementName, nsmgr).InnerText
 
+    //function to get all the elements with a specific name, using mapping. This way we don't have to manually dial the "area code" every time.
     let getElements elementName =
         seq{
             let nodes = xmlNode.SelectNodes("//TEI:" + elementName, nsmgr)
@@ -98,25 +101,56 @@ let main argv =
                 yield node
             }
          |> Seq.map(fun n -> n.InnerText)
+    
+    //function that will use regex to identify numbers in English
+    let regexEnglishNumbers input =
+        let numberRegex = new Regex(@"\b(zero|four|eight|(?:fiv|(?:ni|o)n)e|t(?:wo|hree)|s(?:ix|even)|twelve|(?:(?:elev|t)e|(?:fif|eigh|nine|(?:thi|fou)r|s(?:ix|even))tee)n|(?:fif|six|eigh|nine|(?:tw|sev)en|(?:thi|fo)r)ty|hundred|thousand|million|billion|trillion)\b")
+        //(from http://www.rexegg.com/regex-trick-numbers-in-english.html)
+        //return a list of numbers
+        //MAKE A LIST TO APPEND WORDS TO, SPLIT INPUT INTO WORDS, ADD WORDS TO LIST IF THEY MATCH
+        seq numWords
+        for word in n
+        if numberRegex.IsMatch(word) then
+            numberRegex.append(word)
 
-    let abstractElement = getElement "abstract"
-    let titleElement = getElement "title"
+    //function that will get and run tests on the abstract
+    let abstractTests =
+        let abstractElement = getElement "abstract"
 
-    let absWords = 
-        abstractElement.Split([|' '|]) 
-        |> Array.map (fun s -> s.Trim())
+        //split the abstract chunk into separate words
+        let absWords = 
+            abstractElement.Split([|' '|]) 
+            |> Array.map (fun s -> s.Trim())
 
-    let absWordLength = absWords.Length
-    if absWords.Length > 250 then printfn "Most journals have a word limit between 150 and 250. Your abstract is too long at %i words." absWords.Length
-    if (150 < absWords.Length && absWords.Length < 250) then printfn "Most journals have a word limit between 150 and 250. Your abstract may be too long at %i words. Check with your particular journal." absWords.Length
-    if abstractElement.Contains(titleElement) then printfn "You have a limited number of words. Don't waste them by repeating your title!"
+        //test to see if the abstract is the right length
+        let absWordLength = absWords.Length
+        if absWords.Length > 250 then printfn "Most journals have a word limit between 150 and 250. Your abstract is too long at %i words.\n" absWords.Length
+        if (150 < absWords.Length && absWords.Length < 250) then printfn "Most journals have a word limit between 150 and 250. Your abstract may be too long at %i words. Check with your particular journal.\n" absWords.Length
+
+        let titleElement = getElement "title"
+        if abstractElement.Contains(titleElement) then printfn "You have a limited number of words. Don't waste them by repeating your title!\n"
+
+        //if abstract contains a number it should be a numeral not a word
+        let absNumList = regexEnglishNumbers absWords
+        for word in absNumList do
+            printfn "Numbers in abstracts should be expressed as numerals. Please check the following word: %s\n" word
 
     let paragraphs = getElements "p"
 
-    let references = getElements "ref"
-    let referenceCheck1 = references |> Seq.filter(fun ref -> ref.Contains("and")) // && ref.GetType = "bibr")
+    //function to get and run tests on the references
+    let refTests =
+        let references = getElements "ref"
+        let refsWithAnd = references |> Seq.filter(fun ref -> ref.Contains("and"))
 
-    //if rf.Attribute(xn "type").Value = "bibr" then //no errors? but it's throwing a NullReferenceException
-    //    if (rf.Value.Contains("and") || rf.Value.Contains("&")) then printfn "%s" (rf.Value)
+        for ref in refsWithAnd do
+            printfn "References should contain '&' instead of the word 'and.' Please check the following reference: %s\n" ref
+
+        //if rf.Attribute(xn "type").Value = "bibr" then //no errors? but it's throwing a NullReferenceException
+        //    if (rf.Value.Contains("and") || rf.Value.Contains("&")) then printfn "%s" (rf.Value)
+
+    //biblstruct children "title"
+
+    abstractTests
+    refTests
 
     0 // return an integer exit code
